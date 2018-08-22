b46cca8406baa59751289cb45d9061531772ca24
Siva Kumar Reddy G <yahooda@Sivas-MacBook-Pro.local>
2018-08-22 23:49:03 +0530

    Updated

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,3 +1,3 @@
 ps -aef | grep TodoDemo | grep -v grep | awk '{print $2}' | xargs kill -9
-sleep 10
+sleep 30
 nohup java -Dspring.profiles.active=test -Dserver.port=5000 -jar /opt/app/TodoDemo.war >> /opt/app/app.log &
